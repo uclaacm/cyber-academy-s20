abbd76fd68d1806918e1ea2c3128acf7716e9d9d
Alyssa Wang <alywang27@gmail.com>
2020-04-11 21:49:46 -0700

    add first buffer overflow chall

---
+++ uhoh-go-back/easy1.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+
+#define FLAGSIZE 64
+
+char buf[FLAGSIZE];
+
+void catch() {
+    printf("falg{abcde}");
+    return;
+}
+
+int uhoh() {
+    
+    printf(buf);
+}
+
+void vuln() {
+    char str[16];
+    gets(str);
+    printf("You typed in: %s", str);
+    
+}
+
+int main(int argc, char** argv) {
+    FILE *f = fopen("flag.txt","r");
+    if (f == NULL) {
+        printf("Flag File is Missing. Problem is Misconfigured, please contact an Admin if you are running this on the shell server.\n");
+        exit(0);
+    }
+    fgets(buf,FLAGSIZE,f);
+    vuln();
+    return 0;
+}
