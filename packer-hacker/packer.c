ebccaa9221bfd23236a69af0efd7de8b8a884a5e
William Wang <defund@users.noreply.github.com>
2020-04-13 01:33:21 -0400

    all week 3 challenges

---
+++ packer-hacker/packer.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char f1() { return 'f'; }
+char f2() { return 'l'; }
+char f3() { return 'a'; }
+char f4() { return 'g'; }
+char f5() { return '{'; }
+char f6() { return 'h'; }
+char f7() { return '0'; }
+char f8() { return 'w'; }
+char f9() { return '_'; }
+char f10() { return 'd'; }
+char f11() { return 'a'; }
+char f12() { return 'r'; }
+char f13() { return '3'; }
+char f14() { return '_'; }
+char f15() { return 'y'; }
+char f16() { return '0'; }
+char f17() { return 'u'; }
+char f18() { return '_'; }
+char f19() { return 'u'; }
+char f20() { return 'n'; }
+char f21() { return 'p'; }
+char f22() { return '4'; }
+char f23() { return 'c'; }
+char f24() { return 'k'; }
+char f25() { return '_'; }
+char f26() { return 'm'; }
+char f27() { return 'e'; }
+char f28() { return '}'; }
+
+int main(int argc, char** argv) {
+	printf("I have a secret you'll never know ;)\n");
+}
